226f7d34efadeade59a85c0b9440830656f415f7
Edgardo <emoron@gmail.com>
2015-06-12 08:05:59 -0500

    Json Data
    Se agregaron los arreglos JSON dentro del directorio data y se mueve el
    metodo draggable para agregar dinamicamente elementos

--- js/datos.js
+++ js/datos.js
@@ -1,21 +1,506 @@
-var datos = {
-"array":[ ["-26",	"yx^4",	"8.2",	"x"],
-["0.5/0.3",	"cv^(2/5)",	"5/6",	"nd^4"],
-["34",	"as^4", "c^7", "v^(-23)",	"-9/10",	"h^(-2)", "kz"],
-["0.456",	"xyz^2",	"40",	"gf/kj"],
-["-101",	"m/j^23" ," k",	"4" ,"1/2",	"r^4", "n"],
-["-7/2",	"isl",	"-67",	"t^9", "s"],
-["19",	"x^(34/6)",	"83.1",	"abc"],
-["-94",	"ar^2" ,"s^3",	"45"	,"j/k"],
-["0.14161",	"pq^2", "gq^34",	"0.45",	"l^2" ,"i"],
-["0.45/1.67",	"dl^3" ,"b",	"56.2",	"fpo^(-3)"],
-["-3/5",	"ti^7",	"6",	"q^6", "x^4" ,"z^9"],
-["257",	"osc^(-7/3)",	"732"	,"e^5" ,"nl"],
-["-56.93",	"ar^(-2)",	"0.65",	"xz^2"],
-["5/9",	"lw^2,h^9" ,"v^8",	"2.2"	,"uno"],
-["-65",	"x/y",	"6.23",	"t^(-4)* na"],
-["0.43",	"g^4",	"-5"	,"(i^6 * s^4)/o"],
-["12",	"sjf",	"-5/3",	"c^3 h^(-3)"],
-["5/4",	"h^(-1)op",	"90",	"n"],
-["-4",	"l^3","1000","x^(-9)"]]
-}
+expresiones = 
+	{
+	"modulo":	"1",
+	"actividad":	"1",
+	"expresion":	"-32x^2 y^3",
+	"EjercicioRespuesta":""
+	},
+
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"-5",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"m^5",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"x^4 y^3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"12",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"ab",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"1/3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"s^5 t^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"-4/3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"p q^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"zt",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"78",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"2",
+	"expresion":	"mnp",
+	"EjercicioRespuesta":""
+	},
+
+	{
+	"modulo":	"1",
+	"actividad":	"3",
+	"expresion":	"4 x^5",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"3",
+	"expresion":	"xy/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"3",
+	"expresion":	"-x",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"3",
+	"expresion":	"2/3 x^2 yz",
+	"EjercicioRespuesta":""
+	},
+
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"2 x^3 y^5",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"x y^3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"-7 X^5",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"x^3/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"y+3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"2x^23y",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"y^3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"1",
+	"actividad":	"4",
+	"expresion":	"pi*y",
+	"EjercicioRespuesta":""
+	},
+
+
+	{
+	"modulo":	"2",
+	"actividad":	"1",
+	"expresion":	"3xy+xy^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"2",
+	"actividad":	"1",
+	"expresion":	"-4m^4 n^3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"2",
+	"actividad":	"1",
+	"expresion":	"4ab-5bc+7ac",
+	"EjercicioRespuesta":""
+	},
+
+	{
+	"modulo":	"2",
+	"actividad":	"3",
+	"expresion":	"3xy+xy^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"2",
+	"actividad":	"3",
+	"expresion":	"-4m^4 n^3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"2",
+	"actividad":	"3",
+	"expresion":	"4ab-5bc+7ac",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"2",
+	"actividad":	"3",
+	"expresion":	"xy+1/(2xy^2 )-11xy^2+4x^4 y^3
+",
+	"EjercicioRespuesta":""
+	},
+
+
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"a+b+c+d",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"(a+d)h/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"6x",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"3xy",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"4x",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"x^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"2(x+y)
+",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"1",
+	"expresion":	"xy",
+	"EjercicioRespuesta":""
+	},
+
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"6x+3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"3x+6",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"3(x+6)",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x/3+6",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x/5+10",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x+10)/5",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"10x+5",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"5x+10",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x+7)/4",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x/4+7",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(14+7)/4",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"7/4+x",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(xy)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x+y)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x/2+y",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x-y)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x/2 y",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x/2  y/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x-y)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(xy)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x+y",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x^2 y^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"√(x^2) √(y^2)",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"√(x^2+y^2)",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"0.4x",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"40/100x",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"40/100 x",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"100x/40",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(z+y)^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x^2 + y^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"x+y^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(12+y)^2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x^2+y^2)/4",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x+y^2)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"((x+y)^2)/4",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"((x+y)^2)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"0.5x+0.5y+0.5z",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"((x+y))/2+z)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x+y+z)/3",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x+y+z)/2",
+	"EjercicioRespuesta":""
+	},
+	
+	{
+	"modulo":	"3",
+	"actividad":	"7",
+	"expresion":	"(ab)/2",
+	"EjercicioRespuesta":""
+	},
+	{
+	"modulo":	"3",
+	"actividad":	"3",
+	"expresion":	"(x-y)/10+9",
+	"EjercicioRespuesta":""
+	},
+	
