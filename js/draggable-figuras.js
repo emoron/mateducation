b5a2162aedd8b1acac5cfe75c72e6f965c5f74dc
emoron <emoron@gmail.com>
2015-06-13 00:43:15 -0500

    Actividad 7 - Se muestran las figuras y su caja correspondiente

--- js/draggable-figuras.js
+++ js/draggable-figuras.js
@@ -1,100 +1,100 @@
 var listado = [
   {
-    "Perimetro": "4x",
+    "Perímetro": "4x",
     "figura": "fig_cuadrado.jpg",
     "area": "x^2"
   },
   {
-    "Perimetro": "3f",
+    "Perímetro": "3f",
     "figura": "fig_triangulo.jpg",
     "area": "(fh)/2"
   },
   {
-    "Perimetro": "2a+2b",
+    "Perímetro": "2a+2b",
     "figura": "fig_rectangulo.jpg",
     "area": "ab"
   },
   {
-    "Perimetro": "2r*pi",
+    "Perímetro": "2rpi",
     "figura": "fig_circulo.jpg",
-    "area": "pi*r^2"
+    "area": "pir^2"
   },
   {
-    "Perimetro": "j+2k+l",
+    "Perímetro": "j+2k+l",
     "figura": "fig_trapecio.jpg",
     "area": "(j+l)i/2"
   },
   {
-    "Perimetro": "6k",
+    "Perímetro": "6k",
     "figura": "fig_hexagono.jpg",
     "area": "6kc/2"
   },
   {
-    "Perimetro": "4e",
+    "Perímetro": "4e",
     "figura": "fig_cuadrado2.jpg",
     "area": "e^2"
   },
   {
-    "Perimetro": "2g+2r",
+    "Perímetro": "2g+2r",
     "figura": "fig_rectangulo2.jpg",
     "area": "gr"
   },
   {
-    "Perimetro": "5t",
-    "figura": "fig_pentagono.png",
+    "Perímetro": "5t",
+    "figura": "fig_pentagono.jpg",
     "area": "5tq/2"
   },
   {
-    "Perimetro": "2x*pi",
+    "Perímetro": "2xpi",
     "figura": "circulo2.png",
-    "area": "pi*x^2"
+    "area": "pix^2"
   },
   {
-    "Perimetro": "d+2w+z",
+    "Perímetro": "d+2w+z",
     "figura": "fig_trapecio2.jpg",
     "area": "(d+z)p/2"
   },
   {
-    "Perimetro": "3l",
+    "Perímetro": "3l",
     "figura": "fig_triangulo2.jpg",
     "area": "lb/2"
   },
   {
-    "Perimetro": "5u",
+    "Perímetro": "5u",
     "figura": "fig_pentagono2.jpg",
     "area": "5uv/2"
   },
   {
-    "Perimetro": "4f",
+    "Perímetro": "4f",
     "figura": "fig_cuadrado3.jpg",
     "area": "f^2"
   },
   {
-    "Perimetro": "6s",
+    "Perímetro": "6s",
     "figura": "fig_hexagono2.jpg",
     "area": "6so/2"
   },
   {
-    "Perimetro": "m+2i+k",
+    "Perímetro": "m+2i+k",
     "figura": "fig_trapecio3.jpg",
     "area": "(m+k)p/2"
   },
   {
-    "Perimetro": "a+b+c",
+    "Perímetro": "a+b+c",
     "figura": "fig_triangulo3.jpg",
     "area": "bc/2"
   },
   {
-    "Perimetro": "2h+2n",
+    "Perímetro": "2h+2n",
     "figura": "fig_cuadrado2.jpg",
     "area": "hn"
   }
 ];
 
           var expresiones = [];
-          for(var i = 0 ; i <= 3; i++){
+          for(var i = 0 ; i <= 8; i++){
               var numero = Math.floor(Math.random()*listado.length);
-             console.log(numero);
+             //Console.log(numero);
              var expr1 = listado[numero];
              expresiones.push(expr1);
 
@@ -104,27 +104,16 @@ var listado = [
 
     $.each(expresiones,function(index, valor){
         
-        var identificador = "figura" + index;
-        var objeto ="<div id=\"ecuacion\" class=\"square\">" +
-                " <div id=\"" + identificador + "\"></div>" + 
-               "<p> Area </p>" + '$$' + math.parse(valor.area).toTex() + '$$' +
-                "<p> Perimetro </p>" + '$$' + math.parse(valor.Perimetro).toTex() + '$$'+ "</div></div>";
-        console.log(valor);
+        var identificador = valor.clase + index;
+        var objeto ="<div id=\"monomio\" class=\"square\">" +
+                " <div id=\"" + identificador + "\"></div></div></div>";
 
        $("#objeto").after(""+objeto);    
-       var imagen = valor.figura;
-       
-       console.log(imagen);
-
-    var  imagen = "<div id=\"figuras\" class=\"squaredotted\">" + "<img src=\"img/" + imagen + " \"  style=\"width: 180px\"/>" +"" +  "</div>";
-
-        $("#objeto2").after("" + imagen); 
+      $("TEST").insertBefore("#objecto")
+        var variable =   '$$' + math.parse(valor.expresion).toTex() + '$$';
         
-     //  eval(identificador).innerHTML = variable;
-      
-      // var figura =+
-     //           " <div id=\"" + identificador + "\">;
-     //   eval(identificador).innerHTML = variable;
+       eval(identificador).innerHTML = variable;
+    
     });
 
     
