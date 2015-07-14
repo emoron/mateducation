ca21e90ca7fd94d50403d72acda693c381be12e6
Edgardo <emoron@gmail.com>
2015-07-13 22:29:48 -0500

    Agregando accion a los botones

--- js/memory-2.js
+++ js/memory-2.js
@@ -6,52 +6,105 @@ var ImgFound = 0;
 var Source = "#boxcard";
 
 var ImgSource = [
-  "http://img5.uploadhouse.com/fileuploads/17699/176992640c06707c66a5c0b08a2549c69745dc2c.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699263b01721074bf094aa3bc695aa19c8d573.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699262833250fa3063b708c41042005fda437d.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992615db99bb0fd652a2e6041388b2839a634.png",
-  "http://img4.uploadhouse.com/fileuploads/17699/176992601ca0f28ba4a8f7b41f99ee026d7aaed8.png",
-  "http://img3.uploadhouse.com/fileuploads/17699/17699259cb2d70c6882adc285ab8d519658b5dd7.png",
-  "http://img2.uploadhouse.com/fileuploads/17699/1769925824ea93cbb77ba9e95c1a4cec7f89b80c.png",
-  "http://img7.uploadhouse.com/fileuploads/17699/1769925708af4fb3c954b1d856da1f4d4dcd548a.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992568b759acd78f7cbe98b6e4a7baa90e717.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992554c2ca340cc2ea8c0606ecd320824756e.png"
+  {"expresion": "(0.01g^(-4)", "coeficiente" : "1/0.01", "grado" : 4, "variable": "g"},
+  {"expresion": "-23mj", "coeficiente" : -23, "grado" : 2, "variable": "mj"},
+  {"expresion": "45k^2", "coeficiente" : 45, "grado" : 2, "variable": "k"},
+  {"expresion": "b^4", "coeficiente" : "1", "grado" : 4, "variable": "b"},
+  {"expresion": "hi^3", "coeficiente" : "1", "grado" : 4, "variable": "hi"},
+  {"expresion": "3.4a^4", "coeficiente" : "3.4", "grado" : 4, "variable": "a"},
+  {"expresion": "b^3", "coeficiente" : "1", "grado" : 3, "variable": "b"},
+  {"expresion": "-2/3", "coeficiente" : "-2/3", "grado" : 0, "variable": "null"},
+  {"expresion": "xy^3", "coeficiente" : "1", "grado" : 4, "variable": "xy"},
+  {"expresion": "5/12", "coeficiente" : "5/12", "grado" : 0, "variable": "null"}
 ];
 
 function RandomFunction(MaxValue, MinValue) {
 		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
 	}
 
-function ShuffleImages() {
-	var ImgAll = $(Source).children();
-	var ImgThis = $(Source + " div:first-child");
-	var ImgArr = new Array();
+  function ShuffleImages() {
+  	var ImgAll = $(Source).children();
+  	var ImgThis = $(Source + " div:first-child");
+  	var ImgArr = new Array();
 
-	for (var i = 0; i < ImgAll.length; i++) {
-		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
-		ImgThis = ImgThis.next();
-	}
+  	for (var i = 0; i < ImgAll.length; i++) {
+  		ImgArr[i] = $("#" + ImgThis.attr("id")).html();
+  		ImgThis = ImgThis.next();
+  	}
 
-		ImgThis = $(Source + " div:first-child");
+  		ImgThis = $(Source + " div:first-child");
 
-	for (var z = 0; z < ImgAll.length; z++) {
-	var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+  	for (var z = 0; z < ImgAll.length; z++) {
+  	var RandomNumber = RandomFunction(0, ImgArr.length - 1);
 
-		$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
-		ImgArr.splice(RandomNumber, 1);
-		ImgThis = ImgThis.next();
-	}
+  		$("#" + ImgThis.attr("id")).html(ImgArr[RandomNumber]);
+  		ImgArr.splice(RandomNumber, 1);
+  		ImgThis = ImgThis.next();
+  	}
+  }
+
+  function OpenCard() {
+  	var id = $(this).attr("id");
+
+  //	if ($("#" + id ).is(":hidden")) {
+  		$(Source + " div").unbind("click", OpenCard);
+
+  		$("#" + id).slideDown('fast');
+
+  		if (ImgOpened == "") {
+  			BoxOpened = id;
+  			ImgOpened = $("#" + id ).html();
+  			setTimeout(function() {
+  				$(Source + " div").bind("click", OpenCard)
+  			}, 300);
+  		} else {
+  			CurrentOpened = $("#" + id ).html();
+  			if (ImgOpened != CurrentOpened) {
+  				setTimeout(function() {
+  					$("#" + id + " img").slideUp('fast');
+  					$("#" + BoxOpened + " img").slideUp('fast');
+  					BoxOpened = "";
+  					ImgOpened = "";
+  				}, 400);
+  			} else {
+  				$("#" + id + " img").parent().css("visibility", "hidden");
+  				$("#" + BoxOpened + " img").parent().css("visibility", "hidden");
+  				ImgFound++;
+  				BoxOpened = "";
+  				ImgOpened = "";
+  			}
+  			setTimeout(function() {
+  				$(Source + " div").bind("click", OpenCard)
+  			}, 400);
+  		}
+  		Counter++;
+  		$("#counter").html("" + Counter);
+
+  		if (ImgFound == ImgSource.length) {
+  			$("#counter").prepend('<span id="success">You Found All Pictues With </span>');
+  		}
+  	//}
+  }
+
+
+
+$(function() {
+
+for (var y = 1; y < 3 ; y++) {
+  if(y == 1){
+	$.each(ImgSource, function(i, val) {
+		$(Source).append("<div id=card" + y + i + ">"+ val.expresion + "</div>");
+    	});
+  }else
+  {
+    $.each(ImgSource, function(i, val) {
+      var cadena = "coeficiente: " + val.coeficiente + ", " + "grado: " + val.grado;
+  		$(Source).append("<div id=card" + y + i + ">"+ cadena + "</div>");
+    });
+  }
+
 }
 
-function ResetGame() {
+	$(Source + " div").click(OpenCard);
 	ShuffleImages();
-	$(Source + " div img").hide();
-	$(Source + " div").css("visibility", "visible");
-	Counter = 0;
-	$("#success").remove();
-	$("#counter").html("" + Counter);
-	BoxOpened = "";
-	ImgOpened = "";
-	ImgFound = 0;
-	return false;
-}
+});
