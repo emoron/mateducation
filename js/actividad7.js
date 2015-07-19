10d2e927d1601a0737fe76d15bfe6fe8da230bd3
Edgardo <emoron@gmail.com>
2015-07-19 00:05:54 -0500

    Se alinean las figuras

--- js/actividad7.js
+++ js/actividad7.js
@@ -1,70 +1,164 @@
 var path="img-selecciona/";
 var listado =
-  [{
-  	"figura": "figura07.png",
-  	"area": "(fh)/2",
-  	"expresion": "(12fh)/2",
-  	"sumatoria": "12"
-  },
-  {
-  	"figura": "figura08.png",
-  	"area": "(6fh)/2",
-  	"expresion": "(12fh)/2",
-  	"sumatoria": "12"
-  },
-  {
-  	"figura": "figura09.png",
-  	"area": "(5fh)/2",
-  	"expresion": "(12fh)/2",
-  	"sumatoria": "12"
-  },
-  {
-  	"figura": "figura10.png",
-  	"area": "(fh)/2,",
-  	"expresion": "f^2+(3fh)/2",
-  	"sumatoria": "4"
-  },
-  {
-  	"figura": "figura11.png",
-  	"area": "f^2",
-  	"expresion": "f^2+(3fh)/2",
-  	"sumatoria": "4"
-  },
-  {
-  	"figura": "figura12.png",
-  	"area": "fh",
-  	"expresion": "f^2+(3fh)/2",
-  	"sumatoria": "4"
-  },
-  {
-  	"figura": "figura13.png",
-  	"area": "(pi)h^2",
-  	"expresion": "(pi)h^2+4f^2+2fh",
-  	"sumatoria": "4(pi)+8"
-  }];
+[{
+  "figura": "figura07.png",
+  "area": "(fh)/2",
+  "expresion": "(12fh)/2",
+  "sumatoria": "12"
+},
+{
+  "figura": "figura08.png",
+  "area": "(6fh)/2",
+  "expresion": "(12fh)/2",
+  "sumatoria": "12"
+},
+{
+  "figura": "figura09.png",
+  "area": "(5fh)/2",
+  "expresion": "(12fh)/2",
+  "sumatoria": "12"
+},
+{
+  "figura": "figura10.png",
+  "area": "(fh)/2,",
+  "expresion": "f^2+(3fh)/2",
+  "sumatoria": "4"
+},
+{
+  "figura": "figura11.png",
+  "area": "f^2",
+  "expresion": "f^2+(3fh)/2",
+  "sumatoria": "4"
+},
+{
+  "figura": "figura12.png",
+  "area": "fh",
+  "expresion": "f^2+(3fh)/2",
+  "sumatoria": "4"
+},
+{
+  "figura": "figura13.png",
+  "area": "(pi)h^2",
+  "expresion": "(pi)h^2+4f^2+2fh",
+  "sumatoria": "4(pi)+8"
+},
+{
+  "figura": "figura14.png",
+  "area": "4f^2",
+  "expresion": "(pi)h^2+4f^2+2fh",
+  "sumatoria": "4(pi)+8"
+},
+{
+  "figura": "figura15.png",
+  "area": "2fh",
+  "expresion": "(pi)h^2+4f^2+2fh",
+  "sumatoria": "4(pi)+8"
+},
+{
+  "figura": "figura16.png",
+  "area": "4(pi)f^2",
+  "expresion": "4(pi)f^2+4(pi)h^2+9h^2",
+  "sumatoria": "20(pi)+36"
+},
+{
+  "figura": "figura17.png",
+  "area": "9h^2",
+  "expresion": "4(pi)f^2+4(pi)h^2+9h^2",
+  "sumatoria": "20(pi)+36"
+},
+{
+  "figura": "figura18.png",
+  "area": "4(pi)h^2",
+  "expresion": "4(pi)f^2+4(pi)h^2+9h^2",
+  "sumatoria": "20(pi)+36"
+},
+{
+  "figura": "figura19.png",
+  "area": "(5fh)/2",
+  "expresion": "(62fh)/2",
+  "sumatoria": "62"
+},
+{
+  "figura": "figura20.png",
+  "area": "(3fh)/2",
+  "expresion": "(62fh)/2",
+  "sumatoria": "62"
+},
+{
+  "figura": "figura21.png",
+  "area": "(54fh)/2",
+  "expresion": "(62fh)/2",
+  "sumatoria": "62"
+},
+{
+  "figura": "figura22.png",
+  "area": "3f^2",
+  "expresion": "3f^2+4h^2+(3fh)/2",
+  "sumatoria": "22"
+},
+{
+  "figura": "figura23.png",
+  "area": "4h^2",
+  "expresion": "3f^2+4h^2+(3fh)/2",
+  "sumatoria": "22"
+},
+{
+  "figura": "figura24.png",
+  "area": "(3fh)/2",
+  "expresion": "3f^2+4h^2+(3fh)/2",
+  "sumatoria": "22"
+},
+{
+  "figura": "figura25.png",
+  "area": "fh",
+  "expresion": "(11fh)/2",
+  "sumatoria": "11"
+},
+{
+  "figura": "figura26.png",
+  "area": "(3fh)/2",
+  "expresion": "(11fh)/2",
+  "sumatoria": "11"
+},
+{
+  "figura": "figura27.png",
+  "area": "(6fh)/2",
+  "expresion": "(11fh)/2",
+  "sumatoria": "11"
+},
+{
+  "figura": "figura28.png",
+  "area": "(fh)/2",
+  "expresion": "(30fh)/2",
+  "sumatoria": "30"
+},
+{
+  "figura": "figura29.png",
+  "area": "(20fh)/2",
+  "expresion": "(30fh)/2",
+  "sumatoria": "30"
+},
+{
+  "figura": "figura30.png",
+  "area": "(9fh)/2",
+  "expresion": "(30fh)/2",
+  "sumatoria": "30"
+}];
 
-          var numero = Math.floor(Math.random()*listado.length);
+var valores = [0,4,7,10,13,16,19,21];
 
-          var expresiones = listado.slice(0,3);
-        //  for(var i = 0 ; i <= 2; i++){
-        //
-             // console.log(numero);
-      //       var expr1 = listado[numero];
-        //     expresiones.push(expr1);
-
-      //  }
+          var n = Math.floor(Math.random()*valores.length);
+          var numero = valores[n];
+          var rango = numero + 3;
+          var expresiones = listado.slice(numero,rango);
+          
 
 
 
     $.each(expresiones,function(index, valor){
 
         var identificador = "figura" + index;
-    //    var objeto = " <div id=\"" + identificador + "\"  class=\"square\">" +
-    //           "<p> Area </p>" + '$$' + math.parse(valor.area).toTex() + '$$' +
-    //            "<p> Perimetro </p>" + '$$' + math.parse(valor.expresion).toTex() + '$$'+ "</div>";
-  //      console.log(valor);
 
-  //     $("#objeto").after(""+objeto);
 
        var imagen = "" + path + valor.figura;
 
@@ -74,69 +168,62 @@ var listado =
 
         $("#figuras").append("" + imagen);
 
-     //  eval(identificador).innerHTML = variable;
 
-      // var figura =+
-     //           " <div id=\"" + identificador + "\">;
-     //   eval(identificador).innerHTML = variable;
     });
-
-
-
-
-          // initialize with an example expression
-
 
 
 
 $(function () {
 
-                $(".square").draggable();
-                $("#grafica0").droppable({
-                        accept: "#ecuacion0",
-                        drop: function (event, ui) {
-                                $(this).css("background-color", "lightgreen")
-                                 ui.draggable.remove();
-                        },
-                        out: function (event, ui) {
-                                $(this).css("background-color", "")
-                        }
-                });
+    $('#revisar0').click(function() {
 
 
-              $(".square").draggable();
-                $("#grafica1").droppable({
-                        accept: "#ecuacion1",
-                        drop: function (event, ui) {
-                                $(this).css("background-color", "lightgreen")
-                                 ui.draggable.remove();
-                        },
-                        out: function (event, ui) {
-                                $(this).css("background-color", "")
-                        }
-                });
+      //Checamos las areas de las figuras
+    if(  $('input[name="area1"]').val() == expresiones[0].area ){
+        $('#mensaje1').html('');
+        $('#mensaje1').append("<span class=\"glyphicon glyphicon-ok\"></span>");
+      }
+    else{
+      $('#mensaje1').html('');
+      $('#mensaje1').append("<span class=\"glyphicon glyphicon-remove\"></span>");
+    }
+    if(  $('input[name="area2"]').val() == expresiones[1].area ){
+      $('#mensaje2').html('');
+        $('#mensaje2').append("<span class=\"glyphicon glyphicon-ok\"></span>");
+      }
+    else{
+      $('#mensaje2').html('');
+      $('#mensaje2').append("<span class=\"glyphicon glyphicon-remove\"></span>");
+    }
+    if(  $('input[name="area3"]').val() == expresiones[2].area ){
+      $('#mensaje3').html('');
+        $('#mensaje3').append("<span class=\"glyphicon glyphicon-ok\"></span>");
+      }
+    else{
+      $('#mensaje3').html('');
+      $('#mensaje3').append("<span class=\"glyphicon glyphicon-remove\"></span>");
+    }
+    //Checamos la suma
 
-              $(".square").draggable();
-                $("#grafica2").droppable({
-                        accept: "#ecuacion2",
-                        drop: function (event, ui) {
-                                $(this).css("background-color", "lightgreen")
-                                 ui.draggable.remove();
-                        },
-                        out: function (event, ui) {
-                                $(this).css("background-color", "")
-                        }
-                });
+    if(  $('input[name="expresionSuma"]').val() == expresiones[0].expresion ){
+      $('#mensaje4').html('');
+        $('#mensaje4').append("<span class=\"glyphicon glyphicon-ok\"></span>");
+      }
+    else{
+      $('#mensaje4').html('');
+      $('#mensaje4').append("<span class=\"glyphicon glyphicon-remove\"></span>");
+    }
 
-                $(".square").draggable();
-                $("#grafica3").droppable({
-                        accept: "#ecuacion3",
-                        drop: function (event, ui) {
-                                $(this).css("background-color", "lightgreen")
-                                 ui.draggable.remove();
-                        },
-                        out: function (event, ui) {
-                                $(this).css("background-color", "")
-                        }
-                });
-        });
+    if(  $('input[name="sumatoria"]').val() == expresiones[0].sumatoria ){
+      $('#mensaje5').html('');
+        $('#mensaje5').append("<span class=\"glyphicon glyphicon-ok\"></span>");
+      }
+    else{
+      $('#mensaje5').html('');
+      $('#mensaje5').append("<span class=\"glyphicon glyphicon-remove\"></span>");
+    }
+
+
+    });
+
+});
