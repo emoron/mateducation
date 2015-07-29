5d96144fe0239faace0277500bb957eb78a14fb2
Edgardo <emoron@gmail.com>
2015-07-28 23:42:02 -0500

    Archivos de Expresiones Reubicadas

---
+++ data/json/README.md
@@ -0,0 +1,6 @@
+module2 - Actividades del modulo 2
+mulp.json - contiene multiplicacion y division de polinomios
+operacionesalgebraicas.json - contiene sumas y restas de polinomios
+polinomios - contiene sumas y restas  de polinomios
+productosnotables- Contiene lista de expresiones de productos notables
+factorizacion - contiene expresiones para factorizacion
---
+++ data/json/enunciadoExpresion.js
@@ -0,0 +1,32 @@
+[
+  {"enunciado": "El doble de la sumatoria de 2 números",
+   "expresion":"2∙(x+y)"}
+   {
+     "enunciado":"La tercera parte del producto de 3 números ",
+     "expresion":"(m*n*p)/3"
+   },
+   {
+     "enunciado":"El triple de un número más tres",
+     "expresion":"3∙(x+3)"
+   },
+   {
+     "enunciado" : "Cinco veces el cubo de la suma de un número y el cuatro",
+     "expresion": "5∙(q+4)^3"
+   },
+   {
+     "enunciado" : "La raíz cuadrada del producto de tres números",
+     "expresion" :"sqrt(2&jkl)"
+   },
+   {
+     "enunciado": "La diferencia del tercio de un número y otro número",
+     "expresion":"t/3-u"
+   },
+   {
+     "enunciado":"La cuarta parte del cubo de un número",
+     "expresion" :"r^3/4"
+   },
+   {
+     "enunciado":"El triple de la raíz cuadrada de la sumatoria de dos números",
+     "expresion":"3*sqrt(o+p)"
+   }
+]
